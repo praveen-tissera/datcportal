d48a0c58d5b3b3a9679ad03254e79cb62e6de775
praveen-tissera <praveen.tissera@gmail.com>
2021-02-27 23:29:10 +0530

    month range fix

--- datc_db.sql
+++ datc_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 15, 2021 at 07:19 PM
+-- Generation Time: Feb 27, 2021 at 05:29 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.6
 
@@ -48,10 +48,11 @@ INSERT INTO `batch_table` (`batch_id`, `course_id`, `staff_id`, `batch_number`, 
 (1, 1, 1, '1', '2020-08-08', '2021-08-07', '0000-00-00', '2021-01-03', 'class time : Saturday 9.00 am to 3.00 pm', 'active'),
 (2, 1, 1, '2', '2020-08-09', '2021-08-08', '0000-00-00', '2021-01-03', 'class time : Sunday 9.00 am to 3.00 pm', 'active'),
 (3, 2, 1, '1', '2020-08-16', '2021-08-29', '0000-00-00', '2021-01-03', 'class time : Sunday 9.00 am to 3.00 pm', 'active'),
-(4, 3, 1, '1', '2020-08-08', '2021-08-07', '2021-11-19', '2021-01-03', 'class time : Saturday 9.00 am to 3.00 pm', 'active'),
+(4, 3, 1, '1', '2020-08-08', '2021-08-07', '2021-11-19', '2021-01-03', 'class time : Saturday 9.00 am to 3.00 pm', 'complete'),
 (5, 2, 2, '2', '2020-11-26', '2020-11-26', '2021-01-27', NULL, 'test', 'active'),
 (6, 5, 2, '1', '2020-11-26', '2020-11-26', '2020-12-31', NULL, 'test batch description', 'active'),
-(7, 6, 2, '1', '2020-11-28', '2020-11-30', '2020-12-02', NULL, 'test description', 'active');
+(7, 6, 2, '1', '2020-11-28', '2020-11-30', '2020-12-02', NULL, 'test description', 'active'),
+(8, 7, 2, '1', '2021-01-31', '2021-02-28', '2021-03-03', NULL, 'sadfsadf', 'active');
 
 -- --------------------------------------------------------
 
@@ -77,7 +78,40 @@ INSERT INTO `captcha` (`captcha_id`, `captcha_time`, `ip_address`, `word`) VALUE
 (29, 1610731831, '::1', 'fPDQFU60'),
 (30, 1610731857, '::1', 'zBxRvOHc'),
 (31, 1610731884, '::1', 'Me5QLZAT'),
-(32, 1610731926, '::1', 'INdqthfm');
+(32, 1610731926, '::1', 'INdqthfm'),
+(33, 1611905996, '::1', 'YA7WR0Gf'),
+(34, 1611906002, '::1', 'vODaXOjI'),
+(35, 1611906010, '::1', 'RsHWpe3y'),
+(36, 1611987321, '::1', 'zZVcX0ws'),
+(37, 1611987328, '::1', 'mbAEDzmK'),
+(38, 1611987702, '::1', 'KxZuqMyB'),
+(39, 1612104389, '::1', 'u92wMtzi'),
+(40, 1613236405, '::1', 'SlFba75N'),
+(41, 1613236430, '::1', 'S7JoK0ss'),
+(42, 1613237473, '::1', 'aXx3Flnl'),
+(43, 1613237736, '::1', 'D8xyLwak'),
+(44, 1613237800, '::1', 'RJAFn4R1'),
+(45, 1613237831, '::1', '1KikjlFa'),
+(46, 1613237919, '::1', '9EW3icm0'),
+(47, 1613237986, '::1', 'wF38UUC5'),
+(48, 1613237990, '::1', 'eZ0n5mVJ'),
+(49, 1613238046, '::1', 'vAS9IYx9'),
+(50, 1613238055, '::1', 'rWtVReg7'),
+(51, 1613238190, '::1', 'yiAXxciJ'),
+(52, 1613238199, '::1', 'ChBxl1PD'),
+(53, 1613238406, '::1', 'DHFPn9S0'),
+(54, 1613238747, '::1', 'fKDoefv4'),
+(55, 1613238750, '::1', 'xbmzSLvE'),
+(56, 1613238791, '::1', 'qGUsVcMT'),
+(57, 1613238803, '::1', 'MzAKr7e6'),
+(58, 1613239401, '::1', 'DK5kjkon'),
+(59, 1613494457, '::1', 'wNSZwJV2'),
+(60, 1613877319, '::1', 'cBdK8jqB'),
+(61, 1613877355, '::1', 'i7R5PvrI'),
+(62, 1613877589, '::1', '4oEfN79f'),
+(63, 1613877668, '::1', 'P5abV6K5'),
+(64, 1613918661, '::1', 'NvYCyGJN'),
+(65, 1614436404, '::1', 'fvFaCu8H');
 
 -- --------------------------------------------------------
 
@@ -105,7 +139,8 @@ INSERT INTO `course_table` (`course_id`, `course_name`, `course_description`, `c
 (2, 'Diploma Of Horticulture ', 'The Diploma of Horticulture reflects the role of those who manage amenity horticultural enterprises where a range of skills and knowledge across the breadth of the industry is required or personnel working in horticulture at a level requiring higher technical skills.', '25000.00', 'active', 2, 'diploma', '2020-01-02'),
 (3, 'One Day Training', 'The Diploma of Horticulture reflects the role of those who manage amenity horticultural enterprises where a range of skills and knowledge across the breadth of the industry is required or personnel working in horticulture at a level requiring higher technical skills.', '25000.00', 'active', 2, 'oneday', '2020-01-02'),
 (5, 'test courseon', 'test discript', '40000', 'active', 2, 'diploma', '2020-11-26'),
-(6, 'three days course', 'test', '25000', 'active', 2, 'threedays', '2020-11-28');
+(6, 'three days course', 'test', '25000', 'active', 2, 'threedays', '2020-11-28'),
+(7, 'abc', 'aaldfjlasjf', '50000', 'active', 2, 'threedays', '2021-01-31');
 
 -- --------------------------------------------------------
 
@@ -132,7 +167,14 @@ INSERT INTO `payment_receive_table` (`payment_id`, `receipt_number`, `paid_amoun
 (48, '20201124123716', 25000, '2020-11-24', 2, '2020-11-24'),
 (47, '20201124181924', 15000, '2020-11-24', 2, '2020-11-24'),
 (49, '20201128094722', 25000, '2020-11-28', 2, '2020-11-28'),
-(50, '20210113072936', 30000, '2021-01-13', 2, '2021-01-13');
+(50, '20210113072936', 30000, '2021-01-13', 2, '2021-01-13'),
+(51, '20210129044440', 15000, '2021-01-29', 2, '2021-01-29'),
+(53, '20210129050927', 30000, '2021-01-29', 2, '2021-01-29'),
+(54, '20210131155728', 30000, '2021-01-31', 2, '2021-01-31'),
+(55, '20210131155917', 15000, '2021-01-31', 2, '2021-01-31'),
+(57, '20210131160443', 25000, '2021-01-31', 2, '2021-01-31'),
+(28, '20210216180839', 15000, '2021-02-16', 2, '2021-02-16'),
+(58, '20210216181759', 25000, '2021-02-16', 2, '2021-02-16');
 
 -- --------------------------------------------------------
 
@@ -161,7 +203,15 @@ INSERT INTO `payment_schedule_table` (`payment_id`, `student_id`, `batch_id`, `p
 (47, 37, 2, '2nd installment', 15000, '2022-02-08', '2020-11-21'),
 (48, 38, 4, 'full', 25000, NULL, '2020-11-24'),
 (49, 39, 7, 'full', 25000, NULL, '2020-11-28'),
-(50, 1, 1, 'full', 30000, NULL, '2021-01-13');
+(50, 1, 1, 'full', 30000, NULL, '2021-01-13'),
+(51, 18, 1, '1st installment', 15000, NULL, '2021-01-29'),
+(52, 18, 1, '2nd installment', 15000, '2021-01-27', '2021-01-29'),
+(53, 40, 1, 'full', 30000, NULL, '2021-01-29'),
+(54, 41, 1, 'full', 30000, NULL, '2021-01-31'),
+(55, 42, 1, '1st installment', 15000, NULL, '2021-01-31'),
+(56, 42, 1, '2nd installment', 15000, '2022-02-07', '2021-01-31'),
+(57, 42, 3, 'full', 25000, NULL, '2021-01-31'),
+(58, 41, 4, 'full', 25000, NULL, '2021-02-16');
 
 -- --------------------------------------------------------
 
@@ -185,7 +235,8 @@ CREATE TABLE `staff_table` (
 INSERT INTO `staff_table` (`staff_id`, `staff_name`, `email`, `role_type`, `password`, `state`) VALUES
 (1, 'chathuri', 'chathuri@gmail.com', 'coordinator', '6367c48dd193d56ea7b0baad25b19455e529f5ee', 'active'),
 (2, 'admin', 'admin@gmail.com', 'admin', '6367c48dd193d56ea7b0baad25b19455e529f5ee', 'active'),
-(3, 'online stafff', 'online@datc.com', 'coordinator', '6367c48dd193d56ea7b0baad25b19455e529f5ee', 'active');
+(3, 'online stafff', 'online@datc.com', 'coordinator', '6367c48dd193d56ea7b0baad25b19455e529f5ee', 'active'),
+(4, 'lasitha samarasinhe', 'lasitha@datc.lk', 'coordinator', '6367c48dd193d56ea7b0baad25b19455e529f5ee', 'active');
 
 -- --------------------------------------------------------
 
@@ -235,7 +286,48 @@ INSERT INTO `student_attendance_table` (`student_attent_id`, `student_id`, `batc
 (118, 37, 2, '1', '2021-01-12', '2021-01-11', 2),
 (119, 37, 2, '0', '2021-01-14', '2021-01-11', 2),
 (120, 37, 2, '0', '2021-01-16', '2021-01-11', 2),
-(121, 37, 2, '1', '2021-01-15', '2021-01-11', 2);
+(121, 37, 2, '1', '2021-01-15', '2021-01-11', 2),
+(122, 17, 2, '1', '2021-01-31', '2021-01-31', 2),
+(123, 17, 2, '0', '2021-02-01', '2021-01-31', 2),
+(124, 17, 2, '0', '2021-02-03', '2021-01-31', 2),
+(125, 17, 2, '1', '2021-02-05', '2021-01-31', 2),
+(126, 37, 2, '1', '2021-01-31', '2021-01-31', 2),
+(127, 37, 2, '1', '2021-02-01', '2021-01-31', 2),
+(128, 37, 2, '1', '2021-02-03', '2021-01-31', 2),
+(129, 37, 2, '1', '2021-02-05', '2021-01-31', 2),
+(130, 42, 3, '0', '2021-02-07', '2021-02-13', 2),
+(131, 42, 3, '1', '2021-02-08', '2021-02-13', 2),
+(132, 42, 3, '1', '2021-02-09', '2021-02-13', 2),
+(133, 42, 3, '1', '2021-02-10', '2021-02-13', 2),
+(134, 42, 3, '1', '2021-02-11', '2021-02-13', 2),
+(135, 42, 3, '0', '2021-02-12', '2021-02-13', 2),
+(136, 42, 3, '1', '2021-02-13', '2021-02-13', 2),
+(137, 17, 2, '0', '2021-02-21', '2021-02-21', 2),
+(138, 17, 2, '0', '2021-02-22', '2021-02-21', 2),
+(139, 17, 2, '1', '2021-02-23', '2021-02-21', 2),
+(140, 17, 1, '1', '2021-02-21', '2021-02-25', 2),
+(141, 17, 1, '1', '2021-02-22', '2021-02-25', 2),
+(142, 17, 1, '1', '2021-02-23', '2021-02-25', 2),
+(143, 17, 1, '0', '2021-02-24', '2021-02-25', 2),
+(144, 17, 1, '0', '2021-02-25', '2021-02-25', 2),
+(145, 17, 1, '1', '2021-02-26', '2021-02-25', 2),
+(146, 17, 1, '0', '2021-02-27', '2021-02-25', 2),
+(147, 1, 1, '1', '2021-02-21', '2021-02-25', 2),
+(148, 1, 1, '1', '2021-02-22', '2021-02-25', 2),
+(149, 1, 1, '1', '2021-02-23', '2021-02-25', 2),
+(150, 1, 1, '0', '2021-02-24', '2021-02-25', 2),
+(151, 1, 1, '0', '2021-02-25', '2021-02-25', 2),
+(152, 1, 1, '0', '2021-02-26', '2021-02-25', 2),
+(153, 1, 1, '0', '2021-02-27', '2021-02-25', 2),
+(154, 42, 3, '0', '2021-02-21', '2021-02-25', 2),
+(155, 42, 3, '1', '2021-02-22', '2021-02-25', 2),
+(156, 42, 3, '0', '2021-02-23', '2021-02-25', 2),
+(157, 42, 3, '1', '2021-02-24', '2021-02-25', 2),
+(158, 42, 3, '0', '2021-02-25', '2021-02-25', 2),
+(159, 42, 3, '1', '2021-02-26', '2021-02-25', 2),
+(160, 42, 3, '1', '2021-02-27', '2021-02-25', 2),
+(161, 44, 1, '1', '2021-02-27', '2021-02-25', 2),
+(162, 18, 1, '1', '2021-02-23', '2021-02-26', 2);
 
 -- --------------------------------------------------------
 
@@ -258,12 +350,19 @@ CREATE TABLE `student_batch_map_table` (
 
 INSERT INTO `student_batch_map_table` (`student_id`, `batch_id`, `staff_id`, `added_date`, `state`, `certificate_no`) VALUES
 (17, 2, 3, '2020-08-05', 'active', NULL),
-(17, 1, 3, '2020-08-05', 'pending', '22222222'),
+(17, 1, 3, '2020-08-05', 'active', '22222222'),
 (1, 1, 3, '2020-08-06', 'active', '666'),
-(18, 1, 3, '2020-08-10', 'pending', NULL),
+(18, 1, 3, '2020-08-10', 'active', NULL),
 (37, 2, 2, '2020-11-21', 'active', NULL),
-(38, 4, 2, '2020-11-24', 'active', NULL),
-(39, 7, 2, '2020-11-28', 'active', NULL);
+(38, 4, 2, '2020-11-24', 'active', '22222221'),
+(39, 7, 2, '2020-11-28', 'active', NULL),
+(40, 1, 3, '2021-01-29', 'active', NULL),
+(41, 1, 3, '2021-01-31', 'active', NULL),
+(42, 1, 3, '2021-01-31', 'suspend', NULL),
+(42, 3, 2, '2021-01-31', 'active', NULL),
+(43, 1, 3, '2021-01-31', 'pending', NULL),
+(44, 1, 3, '2021-01-31', 'active', NULL),
+(41, 4, 2, '2021-02-16', 'active', '4444444411');
 
 -- --------------------------------------------------------
 
@@ -285,8 +384,26 @@ CREATE TABLE `student_mark_table` (
 
 INSERT INTO `student_mark_table` (`student_id`, `batch_id`, `mark`, `state`, `subject_id`) VALUES
 (17, 1, 60, 'pass', 1),
-(17, 1, 100, 'pass', 2),
-(17, 1, 0, 'absent', 3);
+(17, 1, 72, 'pass', 2),
+(17, 1, 40, 'fail', 3),
+(17, 1, 70, 'pass', 4),
+(42, 3, 81, 'pass', 7),
+(38, 4, 66, 'pass', 9),
+(17, 2, 79, 'pass', 2),
+(42, 3, 55, 'pass', 10),
+(17, 2, 80, 'pass', 3),
+(17, 2, 70, 'pass', 4),
+(17, 2, 33, 'fail', 5),
+(17, 2, 0, 'absent', 6),
+(17, 1, 55, 'pass', 5),
+(17, 1, 55, 'pass', 6),
+(41, 4, 77, 'pass', 9),
+(1, 1, 60, 'pass', 2),
+(1, 1, 70, 'pass', 3),
+(1, 1, 55, 'pass', 4),
+(1, 1, 0, 'absent', 5),
+(1, 1, 33, 'fail', 6),
+(18, 1, 17, 'fail', 2);
 
 -- --------------------------------------------------------
 
@@ -312,12 +429,17 @@ CREATE TABLE `student_table` (
 --
 
 INSERT INTO `student_table` (`student_id`, `first_name`, `last_name`, `birth_date`, `email`, `telephone`, `password`, `staff_id`, `state`, `register_date`) VALUES
-(1, 'chinthanaa', 'pereraa', '1998-06-04', 'chinthan@gmail.com', '+94764354111', '6367c48dd193d56ea7b0baad25b19455e529f5ee', 1, 'active', '2020-08-01'),
+(1, 'chinthan', 'pereraa', '1998-06-04', 'chinthan@gmail.com', '+94764354111', '6367c48dd193d56ea7b0baad25b19455e529f5ee', 1, 'active', '2020-08-01'),
 (17, 'praveen', 'tissera', '1985-08-03', 'praveen@gmail.com', '+94764354111', '6367c48dd193d56ea7b0baad25b19455e529f5ee', 3, 'active', '2020-08-03'),
-(18, 'lalitha', 'caldera', '2002-02-04', 'lalitha@gmail.com', '0712345644', '6367c48dd193d56ea7b0baad25b19455e529f5ee', 3, 'pending', '2020-08-10'),
+(18, 'lalitha', 'caldera', '2002-02-04', 'lalitha@gmail.com', '0712345644', '6367c48dd193d56ea7b0baad25b19455e529f5ee', 3, 'active', '2020-08-10'),
 (37, 'roshi', 'fernando', '2013-12-31', 'roshi@gmail.com', '+94764354111', '13ef57bf287aadea333199529c11519af8fe4ef7', 2, 'active', '2020-11-21'),
 (38, 'sampa', 'withanachchi', '2005-10-24', 'samparasanie@gmail.com', '12345678', 'd85c89bf4e2cf42fe4cb367394aa81e7479ec41c', 2, 'active', '2020-11-24'),
-(39, '', '', '0000-00-00', '', '', 'da39a3ee5e6b4b0d3255bfef95601890afd80709', 2, 'active', '2020-11-28');
+(39, '', '', '0000-00-00', '', '', 'da39a3ee5e6b4b0d3255bfef95601890afd80709', 2, 'active', '2020-11-28'),
+(40, 'kavi', 'perera', '2000-01-01', 'kavi@gmail.com', '12345678', '6367c48dd193d56ea7b0baad25b19455e529f5ee', 3, 'active', '2021-01-29'),
+(41, 'kasun', 'manchanayaka', '2011-05-09', 'kasun@gmail.com', '12345678', '6367c48dd193d56ea7b0baad25b19455e529f5ee', 3, 'active', '2021-01-31'),
+(42, 'achala', 'upuldeniya', '2012-06-05', 'achala@gamail.com', '12345678', '6367c48dd193d56ea7b0baad25b19455e529f5ee', 3, 'active', '2021-01-31'),
+(43, 'asdfsa', 'sadfasdf', '2021-01-31', 'adsadfsadfasdfmin@gmail.com', '12332232', '6367c48dd193d56ea7b0baad25b19455e529f5ee', 3, 'pending', '2021-01-31'),
+(44, 'kasun', 'chamera', '2021-01-05', 'kasunchamera@gmail.com', '12345678', '6367c48dd193d56ea7b0baad25b19455e529f5ee', 3, 'active', '2021-01-31');
 
 -- --------------------------------------------------------
 
@@ -343,9 +465,10 @@ INSERT INTO `subject_table` (`subject_id`, `course_id`, `subject_name`, `state`)
 (4, 1, 'test2', 'active'),
 (5, 1, 'test3', 'active'),
 (6, 1, 'test4', 'active'),
-(7, 2, 'subject one', 'inactive'),
+(7, 2, 'subject one', 'active'),
 (8, 3, 'a', 'inactive'),
-(9, 3, 'b', 'active');
+(9, 3, 'b', 'active'),
+(10, 2, 'subject two', 'active');
 
 -- --------------------------------------------------------
 
@@ -357,10 +480,27 @@ CREATE TABLE `trainer_attendance_table` (
   `trainer_attend_id` int(11) NOT NULL,
   `trainer_id` int(11) NOT NULL,
   `batch_id` int(11) NOT NULL,
+  `status` enum('1','0') NOT NULL,
   `attend_date` date NOT NULL,
   `added_date` date NOT NULL,
   `staff_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `trainer_attendance_table`
+--
+
+INSERT INTO `trainer_attendance_table` (`trainer_attend_id`, `trainer_id`, `batch_id`, `status`, `attend_date`, `added_date`, `staff_id`) VALUES
+(1, 2, 3, '1', '2021-02-21', '2021-02-25', 2),
+(2, 2, 3, '1', '2021-02-22', '2021-02-25', 2),
+(3, 2, 3, '1', '2021-02-23', '2021-02-25', 2),
+(4, 2, 3, '1', '2021-02-24', '2021-02-25', 2),
+(5, 2, 3, '1', '2021-02-25', '2021-02-25', 2),
+(6, 2, 3, '1', '2021-02-26', '2021-02-25', 2),
+(7, 2, 3, '0', '2021-02-27', '2021-02-25', 2),
+(8, 2, 1, '1', '2021-02-21', '2021-02-25', 2),
+(9, 2, 1, '1', '2021-02-22', '2021-02-25', 2),
+(10, 2, 1, '0', '2021-02-23', '2021-02-25', 2);
 
 -- --------------------------------------------------------
 
@@ -381,9 +521,16 @@ CREATE TABLE `trainer_batch_map_table` (
 --
 
 INSERT INTO `trainer_batch_map_table` (`trainer_id`, `batch_id`, `staff_id`, `added_date`, `state`) VALUES
-(1, 2, 2, '2020-11-25', 'active'),
-(1, 1, 2, '2020-11-26', 'active'),
-(2, 3, 2, '2020-12-30', 'active');
+(1, 2, 2, '2020-11-25', 'deactivate'),
+(2, 3, 2, '2020-12-30', 'active'),
+(2, 1, 2, '2021-02-18', 'deactivate'),
+(2, 1, 2, '2021-02-18', 'active'),
+(1, 5, 2, '2021-02-18', 'active'),
+(2, 5, 2, '2021-02-18', 'deactivate'),
+(2, 6, 2, '2021-02-18', 'active'),
+(2, 8, 2, '2021-02-18', 'deactivate'),
+(1, 8, 2, '2021-02-18', 'active'),
+(2, 1, 1, '2021-02-24', 'deactivate');
 
 -- --------------------------------------------------------
 
@@ -407,8 +554,10 @@ CREATE TABLE `trainer_table` (
 --
 
 INSERT INTO `trainer_table` (`trainer_id`, `first_name`, `last_name`, `birth_date`, `email`, `password`, `state`, `register_date`) VALUES
-(1, 'trainera', 'trainerb', '1987-08-26', 'trainer@gmail.com', 'c9b359951c09c5d04de4f852746671ab2b2d0994', 'active', '2020-08-01'),
-(2, 'praveen', 'tissera', '1981-11-30', 'admin@gmail.com', 'e4bce67fa5c141e90981d3017c21791b11fe7dcc', 'active', '2020-11-24');
+(1, 'traineraa', 'trainerb', '1987-08-26', 'trainer@gmail.com', '6367c48dd193d56ea7b0baad25b19455e529f5ee', 'active', '2020-08-01'),
+(2, 'praveen', 'tissera', '1981-11-30', 'praveen@gmail.com', '6367c48dd193d56ea7b0baad25b19455e529f5ee', 'active', '2020-11-24'),
+(3, 'Dammika', 'Bandara', '1980-01-15', 'dammika@hotmail.com', '6367c48dd193d56ea7b0baad25b19455e529f5ee', 'active', '2021-02-24'),
+(4, 'yonali', 'perera', '2019-06-02', 'yonali@gmail.com', '6367c48dd193d56ea7b0baad25b19455e529f5ee', 'active', '2021-02-25');
 
 --
 -- Indexes for dumped tables
@@ -528,61 +677,61 @@ ALTER TABLE `trainer_table`
 -- AUTO_INCREMENT for table `batch_table`
 --
 ALTER TABLE `batch_table`
-  MODIFY `batch_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `batch_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `captcha`
 --
 ALTER TABLE `captcha`
-  MODIFY `captcha_id` bigint(13) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `captcha_id` bigint(13) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
 
 --
 -- AUTO_INCREMENT for table `course_table`
 --
 ALTER TABLE `course_table`
-  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `payment_schedule_table`
 --
 ALTER TABLE `payment_schedule_table`
-  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
 
 --
 -- AUTO_INCREMENT for table `staff_table`
 --
 ALTER TABLE `staff_table`
-  MODIFY `staff_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `staff_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `student_attendance_table`
 --
 ALTER TABLE `student_attendance_table`
-  MODIFY `student_attent_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=122;
+  MODIFY `student_attent_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=163;
 
 --
 -- AUTO_INCREMENT for table `student_table`
 --
 ALTER TABLE `student_table`
-  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
 --
 -- AUTO_INCREMENT for table `subject_table`
 --
 ALTER TABLE `subject_table`
-  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `subject_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `trainer_attendance_table`
 --
 ALTER TABLE `trainer_attendance_table`
-  MODIFY `trainer_attend_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `trainer_attend_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `trainer_table`
 --
 ALTER TABLE `trainer_table`
-  MODIFY `trainer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `trainer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
